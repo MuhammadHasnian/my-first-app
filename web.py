16c5e19c14308366fbfa0dfb1412cde9ebf429cc
Muhammad Shafique <muhmmadhasnain4801@gmail.com>
2026-03-31 12:46:37 +0100

    Duplicate Error Removed.

--- web.py
+++ web.py
@@ -15,7 +15,7 @@ st.write("This app is to increase your productivity.")
 
 
 for index,todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+    checkbox = st.checkbox(todo,key=index)
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
